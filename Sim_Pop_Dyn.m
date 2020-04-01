df2fffc00ef87284a2c7d753772327790faa321c
Efe Utku <32555442+efeutku@users.noreply.github.com>
2020-04-01 21:03:10 +0200

    Update Sim_Pop_Dyn.m

--- Sim_Pop_Dyn.m
+++ Sim_Pop_Dyn.m
@@ -1,29 +1,29 @@
-pi = 14.85; %recruitment rate
-mu=0.0088; %natural death rate
+pi = 202; %recruitment rate
+mu=0.0089; %natural death rate
 kappa=0.156986; %rate of development of clinical symptoms
 alpha=0.156986; %hospitalization rate of quarantine
 phi=0.20619; %hospitalization rate of infectious
-psi=0.01; %loss of infection acquired immunity
+psi=0.001; %loss of infection acquired immunity
 d1=0.04227; %death rate infectious
 d2=0.027855; %death rate hospitalized
-hnu=.0; %relative infectiousness of hospitalized e.g. 0=perfect intervention
+hnu=.6; %relative infectiousness e.g. 0=perfect intervention
+qnu=.0;
 gamma1=0.03521; %recovery rate of infectious
 gamma2=0.027855; %recovery rate of hospitalized
-gamma3=0.03521;
 mean_cont=0.9575;
 transm_risk=0.20;
 beta=mean_cont*transm_risk;
-st=0.95; %succes rate of test
-ss=0.95; %succes rate of sampling
-rt=1; %result of test e.g. 1=Same day antigen=6, genome=4-5 and higher success
-tr=0.01*rt; %testing rate e.g. T/N, 1=Everybody is tested
+st=0.70; %succes rate of test
+ss=0.782; %succes rate of sampling
+rt=1; %result of test e.g. 1=Same day
+tr=0.14552*rt; %testing rate e.g. T/N, 1=Everybody is tested
 sigma=st*ss*tr; %quarantine rate
 
-N=1500; %population
+N=20000; %population
 
 
-tspan = 0:1:500; % Time Span of the Simulation
-xinit = [1490,0,10,0,0,0,1500]; % Initial Conditons in order; 
+tspan = 0:1:300; % Time Span of the Simulation
+xinit = [19992,0,8,0,0,0,N]; % Initial Conditons in order; 
                         % ('S(t)', 'E(t)', 'I(t)', 'Q(t)', 'H(t)', 'R(t)', 'N(t)')
 
 f = @(t1,y1) [pi+psi*y1(6)-(beta*(y1(3)+hnu*y1(5))/y1(7))*y1(1)-mu*y1(1);
@@ -34,11 +34,16 @@ f = @(t1,y1) [pi+psi*y1(6)-(beta*(y1(3)+hnu*y1(5))/y1(7))*y1(1)-mu*y1(1);
     gamma1*y1(3)+gamma2*y1(5)-(psi+mu)*y1(6);
     pi-mu*y1(7)-(d1*y1(3)+d2*y1(5))];
 
+beds=ones(300,1)* 66.4;
+
 [t1,y1]=ode45(f, tspan, xinit);
 
-%plot(y1(:,:))
-
-%legend('S(t)', 'E(t)', 'I(t)', 'Q(t)', 'H(t)', 'R(t)', 'N(t)')
-plot(y1(:,5))
+plot(y1(:,:))
+xlabel('Days')
+ylabel('Number of Individuals')
+legend('S(t)', 'E(t)', 'I(t)', 'Q(t)', 'H(t)', 'R(t)', 'N(t)')
+%plot(y1(:,5))
 hold on
+%ylim([0 800])
+%plot(beds)
         
